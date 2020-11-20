f62b1f990ad8c61aa07cfc18adc560fface16a55
Nicolas Renet <nicolas@athis.oaklyn>
2020-11-20 11:55:20 -0500

    Clean-up of the lcs module.

--- lcs.py
+++ lcs.py
@@ -11,53 +11,6 @@ Y2= ['C','G','G','T','C','G','A','G']
 X3= ['A','A','G','A','A','T','G','C','G','A']
 Y3= ['C','G','G','A','C','G','A','G']
 
-class Matrix():
-
-    def __init__(self, m,n, default=0, labels=None):
-        self.m=m
-        self.n=n
-        self.m_labels=None
-        self.n_labels=None
-        if labels is not None:
-            self.m_labels=labels[0]
-            self.n_labels=labels[1]
-        self.array = [ [0 for i in range(0,n+1) ] for i in range(0,m+1) ]
-
-    def initialize(self, val ):
-        for i in range(0, self.m):
-            for j in range(0, self.n):
-                self.array[i][j]=val
-    
-
-    def get(self, x,y):
-        return self.array[x][y]
-
-    
-    def set(self, x,y,val):
-        self.array[x][y]=val
-
-    def __str__(self):
-        result = ''
-        if self.n_labels is not None:
-            result += '     y_i'
-            for l in self.n_labels:
-                result += '   {}'.format(l)
-            result += '\n'    
-        for i in range(0,self.m):
-            if self.m_labels is not None:
-                if i==0:
-                    result += ' x_i'
-                else:
-                    result += '   {}'.format(self.m_labels[i-1])
-            for j in range(0,self.n):
-                if len(str(self.array[i][j]))==2:
-                    result += '  '
-                else:
-                    result += '   '    
-                result+='{}'.format(self.array[i][j])
-            result += '\n'
-        return result
-            
 
 def recursive_length( x, y ):
     """
@@ -67,12 +20,7 @@ def recursive_length( x, y ):
         return 0
     if x[-1] == y[-1]:
         return recursive_length( x[0:-1], y[0:-1]) + 1
-    if x[-1] != y[-1]:
-        max_length = recursive_length( x[0:-1],y)
-        length2 = recursive_length( x,y[0:-1])
-        if length2 > max_length:
-            max_length = length2
-        return max_length
+    return max( recursive_length( x[0:-1],y), recursive_length( x,y[0:-1]))
 
 def recursive_lcs( x, y, tab ):
     """
@@ -86,18 +34,11 @@ def recursive_lcs( x, y, tab ):
         lcs = recursive_lcs( x[0:-1], y[0:-1], tab) + x[-1]
         print("{}LCS: {}".format(tab,lcs))
         return lcs
-    if x[-1] != y[-1]:
-        lcs1 = recursive_lcs( x[0:-1],y, tab)
-        lcs2 = recursive_lcs( x,y[0:-1], tab)
-        if len(lcs2) > len(lcs1):
-            lcs1 = lcs2
-        print("{}LCS: {}".format(tab,lcs1))
-        return lcs1
+    return max( recursive_lcs( x[0:-1],y, tab), recursive_lcs( x,y[0:-1], tab))
         
 
     
 def recursive_length_memoized(x, y):
-
 
     def recursive_length_memoized_aux(x, y, r):
         
@@ -108,7 +49,7 @@ def recursive_length_memoized(x, y):
             length = 0
         elif x[-1] == y[-1]:
             length = recursive_length_memoized_aux( x[0:-1], y[0:-1],r) + 1
-        elif x[-1] != y[-1]:
+        else:
             length = recursive_length_memoized_aux( x[0:-1],y, r)
             l2 = recursive_length_memoized_aux( x,y[0:-1], r)
             if l2 > length:
@@ -139,7 +80,7 @@ def non_deterministic_memoized(x, y):
             length += 1
             solutions = [ s + [x[-1]] for s in solutions ] 
             #print(t+ "Solutions: {}".format(solutions))
-        elif x[-1] != y[-1]:
+        else:
             length_1, north_solutions = recursive_memoized_aux( x[0:-1],y, r,t+'  ')
             length_2, west_solutions = recursive_memoized_aux( x,y[0:-1], r,t+'  ')
             if length_2 > length_1:
@@ -164,56 +105,23 @@ def non_deterministic_memoized(x, y):
     
 
 def lcs_length(x, y):
-    m = len(x)
-    n = len(y)
 
-    b = Matrix(m+1, n+1, 0, (x,y))
-    c = Matrix(m+1, n+1, 0, (x,y))
+    m = [  [0 for j in range(len(y)+1) ] for i in range(len(x)+1) ]
 
-    for i in range(1, m+1):
-        
-        for j in range(1, n+1):
-            
+    for i in range(1, len(x)+1):
+        for j in range(1, len(y)+1):
             if x[i-1] == y[j-1]:
-                c.set(i,j, c.get(i-1,j-1) + 1)
-                b.set(i,j,'NW')
-            elif c.get(i-1,j) >= c.get(i,j-1):
-                c.set(i,j,  c.get(i-1,j))
-                b.set(i,j, 'N')
+                m[i][j] = m[i-1][j-1] + 1
+            elif m[i-1][j] >= m[i][j-1]:
+                m[i][j] =  m[i-1][j]
             else:    
-                c.set(i,j, c.get(i, j-1))
-                b.set(i,j, 'W')
-    return (c,b)
+                m[i][j] =  m[i][j-1]
+    return (m[-1][-1], m)
 
 
-def print_lcs(b, X, Y):
+def reconstruct_lcs(c, X, Y):
     """
-    Reconstruct a LCS from matrix B (the "arrows" matrix).
-    
-    :param c: the matrix of lengths
-    :param X: string X
-    :param Y: string Y
-    :return: a list of tokens
-    :rtype: list
-    """
-    def print_lcs_recursive(b,i,j, lst):
-        if i==0 or j==0:
-            return
-        if b.get(i,j)=='NW':
-            print_lcs_recursive(b,i-1,j-1, lst)
-            lst.append('{}'.format(X[i-1]))
-        elif b.get(i,j)=='N':
-            print_lcs_recursive(b,i-1,j, lst)
-        else:
-            print_lcs_recursive(b,i,j-1, lst)
-
-    string = []
-    print_lcs_recursive( b, len(X), len(Y) , string)
-    return string
-
-def print_lcs_alt(c, X, Y):
-    """
-    Reconstruct a LCS without using matrix B (the "arrows" matrix).
+    Reconstruct a LCS from the length matrix.
     
     :param c: the matrix of lengths
     :param X: string X
@@ -222,33 +130,59 @@ def print_lcs_alt(c, X, Y):
     :rtype: list
     """
 
-    active_nodes = [ [ 0 for row in range(0, len(Y)+1) ] for col in range(0, len(X)+1) ]
-
-    def print_lcs_recursive(c, i, j, lst):
-        active_nodes[i][j]=1
+    def reconstruct_lcs_recursive(i, j):
         if i==0 or j==0:
             return
         if X[i-1] == Y[j-1]:
+            reconstruct_lcs_recursive(i-1, j-1)
+            seq.append(X[i-1])
+        elif c[i-1][j] >= c[i][j-1]:
+            reconstruct_lcs_recursive(i-1, j)
+        elif c[i-1][j] < c[i][j-1]:
+            reconstruct_lcs_recursive(i, j-1)
+
+    seq = []
+    reconstruct_lcs_recursive(len(X), len(Y))
+    return seq
+
+def get_active_nodes(c, X, Y):
+    """
+    A matrix of active nodes
+    
+    :param c: the matrix of lengths
+    :param X: string X
+    :param Y: string Y
+    :return: a matrix of lengths
+    :rtype: list
+    """
+    active_nodes = [ [ 0 for col in range(len(Y)+1) ] for row in range(len(X)+1) ]
+
+    def lcs_recursive(i, j):
+        active_nodes[i][j]=1
+        print('lcs_recursive({},{})'.format(i,j))
+        if i==0 or j==0:
+            return
+        if X[i-1] == Y[j-1]:
+            print("Matching character")
             active_nodes[i][j] += 1
-            print_lcs_recursive(c, i-1, j-1, lst)
-            lst.append('{}'.format(X[i-1]))
-        elif c.get(i-1,j) > c.get(i,j-1):
-            print_lcs_recursive(c, i-1, j, lst)
-        elif c.get(i-1,j) == c.get(i,j-1):
-            print_lcs_recursive(c, i, j-1, lst)
-            print_lcs_recursive(c, i-1, j, lst)
+            lcs_recursive(i-1, j-1)
+        elif c[i-1][j] > c[i][j-1]:
+            lcs_recursive(i-1, j)
+        elif c[i-1][j] < c[i][j-1]:
+            lcs_recursive(i, j-1)
         else:
-            print_lcs_recursive(c, i, j-1, lst)
-    string = []
-    print_lcs_recursive(c, len(X), len(Y), string)
-    return (string, active_nodes)
+            lcs_recursive(i, j-1)
+            lcs_recursive(i-1, j)
+
+    lcs_recursive(len(X), len(Y))
+    return active_nodes
           
 
 def lcs_to_tikz(x, y, template=False):
 
-    c,b = lcs_length(x, y)
+    length,c = lcs_length(x, y)
 
-    s,active_nodes = print_lcs_alt(c, x, y)
+    active_nodes = get_active_nodes(c, x, y)
 
     active_edges=[]
     output = []
@@ -256,7 +190,7 @@ def lcs_to_tikz(x, y, template=False):
     output.append( '\\begin{tikzpicture}[-,>=stealth\',auto,node distance=2.8cm, semithick    ]')
     output.append( '\\tikzset{')
     output.append( 'optimal/.style = {preaction={draw,red,-,double=red,double distance=3\pgflinewidth}, draw=black},')
-    output.append( 'letter/.style= {font=\ttfamily\normalsize},')
+    output.append( 'letter/.style= {font=\\ttfamily\\normalsize},')
     output.append( 'highlight/.style = {fill=blue!20,draw},')
     output.append( 'select/.style = {fill=red!30,draw}')
     output.append('}')
@@ -282,9 +216,9 @@ def lcs_to_tikz(x, y, template=False):
     for col in range(2,last_col+1):
         rw = 0
         if active_nodes[0][col-2] and not template:
-            output.append( '\\node\t({}-{})[highlight] at ({},{}*.75)\t{{${}$}};'.format(col, top_row-2, col, top_row-2, c.get(0,col-2)))
+            output.append( '\\node\t({}-{})[highlight] at ({},{}*.75)\t{{${}$}};'.format(col, top_row-2, col, top_row-2, c[0][col-2]))
         else:
-            output.append( '\\node\t({}-{}) at ({},{}*.75)\t{{${}$}};'.format(col, top_row-2, col, top_row-2, c.get(0,col-2)))
+            output.append( '\\node\t({}-{}) at ({},{}*.75)\t{{${}$}};'.format(col, top_row-2, col, top_row-2, c[0][col-2]))
 
     for row in range(top_row-3,-1,-1):
         rw = top_row-(2+row)
@@ -296,9 +230,9 @@ def lcs_to_tikz(x, y, template=False):
             if active_nodes[rw][cl] and not template:
                 #print('tikz (x,y)={},{} --> [{},{}]'.format(row, col, rw,cl))
                 if active_nodes[rw][cl]==1:
-                    output.append( '\\node[highlight]\t({}-{}) at ({},{}*.75)\t{{${}$}};'.format(col, row, col, row, c.get(rw,cl)))
+                    output.append( '\\node[highlight]\t({}-{}) at ({},{}*.75)\t{{${}$}};'.format(col, row, col, row, c[rw][cl]))
                 elif active_nodes[rw][cl]==2:
-                    output.append( '\\node[select]\t({}-{}) at ({},{}*.75)\t{{$\\mathbf{{{}}}$}};'.format(col, row, col, row, c.get(rw,cl)))
+                    output.append( '\\node[select]\t({}-{}) at ({},{}*.75)\t{{$\\mathbf{{{}}}$}};'.format(col, row, col, row, c[rw][cl]))
                 # explore W, NW, and N adjacent nodes: stores active edges
                 if active_nodes[rw][cl]==2 and cl>0 and rw>0 and active_nodes[rw-1][cl-1]:
                     active_edges.append( '({}-{}) edge ({}-{})'.format( col-1, row+1, col, row ))
@@ -308,7 +242,7 @@ def lcs_to_tikz(x, y, template=False):
                     if rw>0 and active_nodes[rw-1][cl]:
                         active_edges.append( '({}-{}) edge ({}-{})'.format( col, row+1, col, row ))
             else:
-                output.append('\\node\t({}-{}) at ({},{}*.75)\t{{\\tt {}}};'.format(col, row, col, row, c.get(rw, cl)))    
+                output.append('\\node\t({}-{}) at ({},{}*.75)\t{{\\tt {}}};'.format(col, row, col, row, c[rw][ cl]))    
     
     output.append('\\path')
     output.append( '\n'.join(active_edges))
@@ -324,25 +258,6 @@ def lcs_to_tikz(x, y, template=False):
     return '\n'.join(output)
 
 
-def sample_adn(l):
-
-    proteins = [ 'A','C','G','T']
-
-    l2 = l - random.randint(0,4)
-
-    x = []
-    y = []
-
-    for i in range(l):
-        x.append( proteins[ random.randint(0,3) ])
-        
-    for i in range(l2):
-        y.append( proteins[ random.randint(0,3) ])
-    return (x,y)
-        
-    
-
-
 class LCS_Test( unittest.TestCase ):
 
 
@@ -355,70 +270,59 @@ class LCS_Test( unittest.TestCase ):
     X3= ['A','A','G','A','A','T','G','C','G','A']
     Y3= ['C','G','G','A','C','G','A','G']
 
-    def atest_lcs_to_tikz(self):
+    def test_lcs_to_tikz(self):
         print( lcs_to_tikz( ['A','C','G','A'], ['A','T','G','C','T','A']))
 
-    def atest_lcs_length_1( self ):
-        matrices = lcs_length( self.X1, self.Y1 )
-        print(matrices[0])
-        print(matrices[1])
-        self.assertEqual(print_lcs( matrices[1], self.X1, self.Y1), ['B','C','B','A'])
+    def test_lcs_length_1( self ):
+        length,matrix = lcs_length( self.X1, self.Y1 )
+        print(length, matrix)
+        self.assertEqual(reconstruct_lcs( matrix, self.X1, self.Y1), ['B','D','A','B'])
 
 
-    def atest_lcs_length_2( self ):
-        matrices = lcs_length( self.X2, self.Y2 )
-        print(matrices[0])
-        print(matrices[1])
-        self.assertEqual(print_lcs( matrices[1], self.X2, self.Y2), ['C','G','G','T','G','G'])
+    def test_lcs_length_2( self ):
+        length,matrix = lcs_length( self.X2, self.Y2 )
+        self.assertEqual(reconstruct_lcs( matrix, self.X2, self.Y2), ['C','G','G','T','G','G'])
 
 
-    def atest_lcs_length_3( self ):
-        matrices = lcs_length( self.X3, self.Y3 )
-        print(matrices[0])
-        print(matrices[1])
-        self.assertEqual(print_lcs( matrices[1], self.X3, self.Y3), ['G','A','C','G','A'])
+    def test_lcs_length_3( self ):
+        length,matrix = lcs_length( self.X3, self.Y3 )
+        self.assertEqual(reconstruct_lcs( matrix, self.X3, self.Y3), ['G','A','C','G','A'])
 
-    def atest_recursive_length_1( self ):
+    def test_recursive_length_1( self ):
         self.assertEqual( recursive_length(self.X1, self.Y1), 4 )
         
-    def atest_recursive_length_2( self ):
+    def test_recursive_length_2( self ):
         self.assertEqual( recursive_length(self.X2, self.Y2), 6 )
 
-    def atest_recursive_length_3( self ):
+    def test_recursive_length_3( self ):
         self.assertEqual( recursive_length(self.X3, self.Y3), 5 )
 
 
-    def atest_recursive_length_memoized_1( self ):
+    def test_recursive_length_memoized_1( self ):
         self.assertEqual( recursive_length_memoized(self.X1, self.Y1), 4 )
         
-    def atest_recursive_length_memoized_2( self ):
+    def test_recursive_length_memoized_2( self ):
         self.assertEqual( recursive_length_memoized(self.X2, self.Y2), 6 )
 
-    def atest_recursive_length_memoized_3( self ):
+    def test_recursive_length_memoized_3( self ):
         self.assertEqual( recursive_length_memoized(self.X3, self.Y3), 5 )
 
 
-    def atest_15_4_1_1( self ):
+    def test_15_4_1_1( self ):
         X = [ '0', '1', '0', '1', '1', '0', '1', '1', '0' ]
         Y = [ '1', '0', '0', '1', '0','1','0','1']
-        matrices = lcs_length( X, Y )
-        print(matrices[0])
-        print(matrices[1])
-        self.assertEqual(print_lcs( matrices[1], X, Y),['0','1','0','1','0','1'])
+        length, matrix = lcs_length( X, Y )
+        self.assertEqual( reconstruct_lcs(matrix, X, Y),['0','1','0','1','0','1'])
 
-    def atest_15_4_1_2( self ):
+    def test_15_4_1_2( self ):
         X = [ '0', '1', '0', '1', '1', '0', '1', '1', '0' ]
         Y = [ '1', '0', '0', '1', '0','1','0','1']
-        matrices = lcs_length( Y, X )
-        print(matrices[0])
-        print(matrices[1])
-        self.assertEqual(print_lcs( matrices[1], Y, X),['1','0','0','1','1','0'])
+        length, matrix = lcs_length( Y, X )
+        self.assertEqual(reconstruct_lcs( matrix, Y, X),['1','0','0','1','1','0'])
 
-    def atest_15_4_2_1( self):
-        matrices = lcs_length( self.X3, self.Y3 )
-        print(matrices[0])
-        print(matrices[1])
-        self.assertEqual(print_lcs_alt( matrices[0], self.X3, self.Y3), ['G','A','C','G','A'])
+    def test_15_4_2_1( self):
+        length, matrix = lcs_length( self.X3, self.Y3 )
+        self.assertEqual(reconstruct_lcs( matrix, self.X3, self.Y3), ['G','A','C','G','A'])
 
 #    def atest_recursive_lcs_1( self ):
 #        self.assertEqual( recursive_lcs(self.X1, self.Y1, ''), 'BCBA' )
@@ -426,7 +330,6 @@ class LCS_Test( unittest.TestCase ):
 #    def atest_recursive_lcs_2( self ):
 #        self.assertEqual( recursive_lcs(self.X2, self.Y2, ''), 'CGGTGG' )
 #
-#    def atest_recursive_lcs_3( self ):
 #        self.assertEqual( recursive_lcs(self.X3, self.Y3, ''), 'GACGA' )
 
 
@@ -450,6 +353,8 @@ class LCS_Test( unittest.TestCase ):
 #    print_lcs(matrices[1], pair[0], len(pair[0]), len(pair[1]), result)
 #
 #    print(result)
+
+
 
 
 
